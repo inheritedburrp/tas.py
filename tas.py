830b102876e6138e5d26d4383c3b0be287b14796
Anuj <anuj.want002@gmail.com>
2015-01-10 18:40:48 +0530

    Update tas.py

--- tas.py
+++ tas.py
@@ -27,3 +27,23 @@ def nginxcheck():
 		subprocess.Popen("sudo add-apt-repository ppa:nginx/stable",shell=True).wait()
 		subprocess.Popen("sudo apt-get update",shell=True).wait()
 		subprocess.Popen("sudo apt-get install nginx-full",shell=True).wait()
+def hosts_entry(domain_name):
+	f=open("/etc/hosts","r+")
+	dom=f.readlines()
+	for dmn in dom:
+		if domain_name in dmn:
+			print ("Domain name already exist in /etc/hosts, proceeding further...")
+			con=True
+			break
+	
+def main():
+	usercheck()
+	phpcheck()
+	mysqlcheck()
+	nginxcheck()
+	print("Enter the Domain Name")
+	domain_name = str(input(">>"))
+	localhost_ip=str([(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1])
+	print("your localhost ip is:\n%s"%localhost_ip)
+	hosts_name()
+main()
